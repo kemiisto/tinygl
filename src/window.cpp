96cacac85cca159e4e78b69b74ab4a05ca6e14fd
kemiisto <kemiisto@gmail.com>
2022-06-15 20:42:22 +0300

    Remove comments

--- src/window.cpp
+++ src/window.cpp
@@ -65,37 +65,13 @@ struct tinygl::Window::WindowPrivate
 tinygl::Window::Window(int width, int height, const std::string& title) :
         p{std::make_unique<WindowPrivate>()}
 {
-    /**
-     * int glfwInit()
-     *
-     * Before most GLFW functions can be used, GLFW must be initialized by glfwInit call.
-     * And before an application terminates GLFW should be terminated by glfwTerminate call.
-     *
-     * Note though, that if glfwInit fails, it calls glfwTerminate before returning.
-     * So you should call glfwTerminate before the application exits only if glfwInit succeeds.
-     *
-     * Returns GLFW_TRUE (1) if successful, or GLFW_FALSE (0) if an error occurred.
-     */
     if (!glfwInit()) {
         throw std::runtime_error("glfwInit() failed!");
     }
-    /**
-     * void glfwWindowHint(int hint, int value)
-     *
-     * Sets hints for the next call to glfwCreateWindow.
-     * The first argument is the option we want to configure.
-     * The second argument is an integer value of the option.
-     */
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    /**
-     * https://www.khronos.org/opengl/wiki/OpenGL_Context#Forward_compatibility
-     *
-     * Forward compatibility will cause, for the given profile, all functionality marked "deprecated" to be removed.
-     * Recommendation: You should use the forward compatibility bit only if you need compatibility with macOS.
-     * That API requires the forward compatibility bit to create any core profile context.
-     */
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
@@ -103,15 +79,6 @@ tinygl::Window::Window(int width, int height, const std::string& title) :
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 #endif
 
-    /**
-     * GLFWwindow* glfwCreateWindow(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share)
-     *
-     * Creates a window and its associated OpenGL or OpenGL ES context.
-     * Successful creation does not change which context is current.
-     * Before you can use the newly created context, you need to make it current.
-     *
-     * Returns the handle of the created window, or NULL if an error occurred.
-     */
     p->window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
     if (!p->window) {
         throw std::runtime_error("glfwCreateWindow() failed!");
@@ -122,19 +89,7 @@ tinygl::Window::Window(int width, int height, const std::string& title) :
     // see setKeyCallback() method
     glfwSetWindowUserPointer(p->window, reinterpret_cast<void*>(this));
 
-    /**
-     * https://www.khronos.org/opengl/wiki/OpenGL_Loading_Library#Initialization_of_GLEW_1.13.0_and_earlier
-     *
-     * GLEW up to version 1.13.0 has a problem with core contexts.
-     * The only fix for earlier versions is to set glewExperimental to GL_TRUE before calling glewInit().
-     */
     glewExperimental = GL_TRUE;
-    /**
-     * glewInit creates a valid OpenGL rendering context and initializes the extension entry points.
-     * Returns GLEW_OK if succeeded so you can use the available extensions as well as core OpenGL functionality.
-     * Somewhat strangely GLEW_OK is defined to be 0, so the idiomatic if (!glewInit()) {...} won't work!
-     * if (glewInit()) {...} will of course, but one would better use a more lengthy but explicit condition.
-     */
     if (glewInit() != GLEW_OK) {
         throw std::runtime_error("glewInit() failed!");
     }
@@ -175,11 +130,6 @@ tinygl::Window::Window(int width, int height, const std::string& title) :
 tinygl::Window::~Window()
 {
     if (p->window) {
-        /**
-         * void glfwDestroyWindow(GLFWwindow* window)
-         *
-         * Destroys the specified window and its context.
-         */
         glfwDestroyWindow(p->window);
     }
 }
