b8ad54163cedafde84f9c3632e0754bf26de02e3
kemiisto <kemiisto@gmail.com>
2023-03-09 20:57:10 +0300

    feat: add new shader types

--- include/tinygl/shader.h
+++ include/tinygl/shader.h
@@ -13,7 +13,11 @@ namespace tinygl
     public:
         enum class Type {
             Vertex = GL_VERTEX_SHADER,
-            Fragment = GL_FRAGMENT_SHADER
+            Fragment = GL_FRAGMENT_SHADER,
+            Geometry = GL_GEOMETRY_SHADER,
+            TessellationControl = GL_TESS_CONTROL_SHADER,
+            TessellationEvaluation = GL_TESS_EVALUATION_SHADER,
+            Compute = GL_COMPUTE_SHADER
         };
 
         Shader(Type type, const char* source);
